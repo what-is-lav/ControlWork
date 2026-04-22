dfd9d160116f6d4dbde043d696cbad9058b1e0c6
uhuieee <ivlad9454@gmail.com>
2026-04-22 17:48:34 +0600

    КонтрÑольнÑая работа

---
+++ oop.py
@@ -0,0 +1,83 @@
+import math
+from abc import ABC, abstractmethod
+class Person:
+    def init(self):
+        self._age = 0
+    def set_age(self, age):
+        if age < 0:
+            print("ошибка: возраст не может быть отрицательным")
+        else:
+            self._age = age
+    def get_age(self):
+        return self._age
+
+
+class Animal:
+    def init(self, name):
+        self.name = name
+    def speak(self):
+        return "I am an animal"
+
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+
+class Vehicle:
+    def move(self):
+        return "Vehicle is moving"
+
+
+class Car(Vehicle):
+    def move(self):
+        return "Car is driving"
+
+
+class Bicycle(Vehicle):
+    def move(self):
+        return "Bicycle is pedaling"
+def move(vehicle):
+    return vehicle.move()
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def init(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+
+
+class Circle(Shape):
+    def init(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * (self.radius ** 2)
+p = Person()
+p.set_age(25)
+print(p.get_age())
+p.set_age(-5)
+dog = Dog("Buddy")
+cat = Cat("Kitty")
+print(dog.name, dog.speak())
+print(cat.name, cat.speak())
+car = Car()
+bike = Bicycle()
+print(move(car))
+print(move(bike))
+rect = Rectangle(10, 5)
+circle = Circle(7)
+print(rect.area())
+print(round(circle.area(), 2))
